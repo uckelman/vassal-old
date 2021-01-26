02b55a50ae227cebc43814da7a3d1021b931bade
Tobin Bergen-Hill <TBERGENHILL@MITRE.ORG>
2021-01-26 10:54:11 -0500

    Added missing libraries and manifest; corrected classpath and bundle script

--- bundle.sh
+++ bundle.sh
@@ -4,14 +4,15 @@ rm -f lib/Vengine.jar
 jar cmf manifest.mf lib/Vengine.jar -C bin .
 echo Setting up VASSAL distribution directory "/c/temp/VASSAL"
 rm -rf /c/temp/VASSAL
+rm -f /c/temp/VASSAL.zip
 mkdir /c/temp/VASSAL
 cp -r lib /c/temp/VASSAL
 cp dist/windows/VASSAL.* /c/temp/VASSAL
+cp dist/windows/VASSAL_server.bat /c/temp/VASSAL
 cp lib-nondist/*.jar /c/temp/VASSAL/lib
 cp src/logback.xml /c/temp/VASSAL
 cp src/vassal.xml /c/temp/VASSAL
 cp -r doc /c/temp/VASSAL
-cp VASSAL_server.bat /c/temp/VASSAL
 echo Changing directories
 pushd /c/temp/ 
 echo Zipping up directory for distribution
